e04bf165cc513c0208c21bc31565878494cac56b
TossiKim <dydrkfk22@gmail.com>
2025-12-07 15:38:28 +0900

    main페이지 수정

--- src/main/java/kr/ac/kopo/kyg/projectkyg/controller/MainController.java
+++ src/main/java/kr/ac/kopo/kyg/projectkyg/controller/MainController.java
@@ -1,14 +1,28 @@
 package kr.ac.kopo.kyg.projectkyg.controller;
-
+import jakarta.servlet.http.HttpSession;
+import kr.ac.kopo.kyg.projectkyg.domain.Member;
+import kr.ac.kopo.kyg.projectkyg.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+// Spring Security의 Authentication 객체를 사용하기 위해 SecurityContextHolder 추가
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.Authentication;
 
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+    private final MemberService memberService;
 
-    // "/"와 "/main.html" 모두 main 페이지 렌더링
     @GetMapping({"/", "/main"})
-    public String main() {
-        return "main";  // templates/main.html
+    public String main(HttpSession session, Model model) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String memberId = authentication.getName();
+        Member member = memberService.getMemberByMemberId(memberId);
+        model.addAttribute("memberName", member.getMemberName());
+
+        return "main";
     }
 }
